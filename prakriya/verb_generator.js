787e4a680940d87c22ec2d63d451334b6de58fdf
alvarna <avinash.l.varna@intel.com>
2017-12-17 17:23:16 -0700

    Use API that returns all info and display more information

--- prakriya/verb_generator.js
+++ prakriya/verb_generator.js
@@ -1,54 +1,80 @@
-//"use strict";
 // write small plugin for keypress enter detection
 $.fn.enterKey = function (fnc) {
+    "use strict";
     return this.each(function () {
         $(this).keypress(function (e) {
             var keycode = (e.keyCode ? e.keyCode : e.which);
-            if (keycode == "13") {
+            if (keycode === 13) {
                 fnc.call(this, e);
             }
         });
     });
 };
 
-function createPanel(heading, row, id, outputTrans) {
+function createSubPanel(result, id) {
+    "use strict";
+    var h = "<div class=\"card\">";
+    h += "<div class=\"card-header\">";
+    h += "<a class=\"\" data-toggle=\"collapse\" href=\"#collapseSub" + id + "\" aria-expanded=\"false\" aria-controls=\"collapseSub" + id + "\">";
+    h += "Additional Information</a></div>"; //card-header
+    h += "<div id=\"collapseSub" + id + "\" class=\"collapse \">";
+    h += "<table class=\"table table-striped\"><tbody>";
+    var links = ["madhaviya", "dhatupradipa", "kshiratarangini", "jnu", "uohyd"];
+    links.forEach(function (prop) {
+        h += "<tr><td>" + prop + "</td>";
+        h += "<td><a href=" + result[prop] + ">Link</a></td>";
+    });
+    h += "</tbody></table>";
+    h += "</div>"; // collapse
+    h += "</div>"; //card
+    return h;
+}
+
+function createPanel(result, id) {
+    "use strict";
     var cardClass = id % 2 ? "bg-secondary" : "bg-primary";
     var expanded = id === 0 ? "show" : "";
-    var h = "<div class=\"card\"><div class=\"card-header " + cardClass + "\">";
-    h += "<a class=\"text-white\" data-toggle=\"collapse\" href=\"#collapse" + id + "\" aria-expanded=\"false\" aria-controls=\"collapse" + id + "\">";
-    h += heading + "</a></div>";
-    h += "<div id=\"collapse" + id + "\" class=\"collapse " + expanded + "\"><div class=\"card-block\">";
+    var heading = "";
+    heading = result.verb + " " + result.meaning + " ";
+    heading += "(" + result.gana + " " + result.it_status + ") - ";
+    heading += result.lakara + " ";
+    heading += result.purusha + ". " + result.vachana + ". ";
+
+    var h = "<div class=\"card\">";
+    h += "<div class=\"card-header " + cardClass + "\">";
+    h += "<a class=\"text-white\" data-toggle=\"collapse\" href=\"#collapse" + id;
+    h += "\" aria-expanded=\"false\" aria-controls=\"collapse" + id + "\">";
+    h += heading + "</a></div>"; //card-header
+    h += "<div id=\"collapse" + id + "\" class=\"collapse " + expanded + "\">";
     h += "<table class=\"table table-striped\">";
     h += "<thead><th scope=\"col\">Sutra</th><th scope=\"col\">Effect</th></thead><tbody>";
-    row.forEach(function (entry) {
-        var item = entry.map(function (e) {
-            var temp = e.replace("!","~");
-            console.log(e);console.log(temp);
-            return temp;
-        });
+    result.derivation.forEach(function (item) {
         h += "<tr><td>";
         h += item[0] + " (" + item[1] + ")</td>";
         h += "<td>" + item[2] + "</td></tr>";
     });
     h += "</tbody></table>";
-    h += "</div></div></div>";
+
+    h += createSubPanel(result, id);
+    h += "</div>"; // collapse
+    h += "</div>"; //card
     return h;
 }
 
 $(document).ready(function () {
+    "use strict";
     $("#goButton").on("click", function () {
         var inputTrans = $("#inputTrans").val();
         var urlbase = $.query.get("api_url_base") !== "" ? $.query.get("api_url_base")
-            : "https://api.sanskritworld.in/v0.0.2/verbforms/" + inputTrans + '/';
+            : "https://api.sanskritworld.in/v0.0.2/verbforms/" + inputTrans + "/";
         var btn = $(this);
         var btxt = btn.text();
         btn.removeClass("btn-primary").addClass("btn-secondary");
         btn.text("Loading ...");
         $("#issueButton").addClass("d-none");
         var inputText = $("#inputText").val();
-        var txt = inputText;
         var outputTrans = $("#outputTrans").val();
-        var url = urlbase + txt + "/prakriya/machine?output_transliteration=" + outputTrans;
+        var url = urlbase + inputText + "?output_transliteration=" + outputTrans;
         console.log(url);
         $.getJSON(url, function (result) {
             var s = JSON.stringify(result);
@@ -63,7 +89,7 @@ $(document).ready(function () {
                 $("#reshead").text("Prakriyaa");
                 var panelID = 0;
                 result.forEach(function (res) {
-                    restable += createPanel("Prakriyaa " + (panelID + 1), res, panelID, outputTrans);
+                    restable += createPanel(res, panelID);
                     panelID += 1;
                 });
             } else if (result.hasOwnProperty("error")) {
