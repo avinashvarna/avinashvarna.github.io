5bd07416881cdff4483b588339fd3432e4da051b
Dr. Dhaval Patel <drdhaval2785@gmail.com>
2018-01-07 19:54:40 +0530

    change JS for key change in API

--- prakriya/verb_generator.js
+++ prakriya/verb_generator.js
@@ -49,10 +49,11 @@ function createPanel(result, id) {
     h += "<div id=\"collapse" + id + "\" class=\"collapse " + expanded + "\">";
     h += "<table class=\"table table-striped\">";
     h += "<thead><th scope=\"col\">Sutra</th><th scope=\"col\">Effect</th></thead><tbody>";
-    result.derivation.forEach(function (item) {
+    console.log(result['prakriya']);
+    result.prakriya.forEach(function (item) {
         h += "<tr><td>";
-        h += item[0] + " (" + item[1] + ")</td>";
-        h += "<td>" + item[2] + "</td></tr>";
+        h += item['sutra'] + " (" + item['sutra_num'] + ")</td>";
+        h += "<td>" + item['form'] + "</td></tr>";
     });
     h += "</tbody></table>";
 
