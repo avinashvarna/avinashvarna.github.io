6ab8c1813f24fa8c3ee363a32d439c6233670c97
alvarna <avinash.l.varna@intel.com>
2018-01-11 07:37:51 -0700

    Updated UI to allow queries and generate static link

--- prakriya/verb_generator.js
+++ prakriya/verb_generator.js
@@ -72,61 +72,82 @@ function createPanel(result, id) {
     return h;
 }
 
+function getPrakriya(inputTrans, inputText, outputTrans) {
+    var urlbase = $.query.get("api_url_base") !== "" ? $.query.get("api_url_base") :
+            "https://api.sanskritworld.in/v0.0.2/verbforms/" + inputTrans + "/";
+    var url = urlbase + inputText + "?output_transliteration=" + outputTrans;
+    
+    var btn = $("#goButton");
+    btn.removeClass("btn-primary").addClass("btn-secondary");
+    btn.text("Loading ...");
+    $("#devinp").text(inputText);
+    $("#jsonbox").text("");
+    $("#jsonButton").addClass("d-none");
+    $("#linktab").addClass("d-none");
+    $("#restab").addClass("d-none");
+    $("#restable").html("");
+    $.getJSON(url, function (result) {
+        var s = JSON.stringify(result);
+        $("#jsonbox").text(s);
+        $("#restable").html("");
+        //console.log(result);
+        var restable = "";
+        if (Array.isArray(result)) {
+            restable += "<strong>Found " + result.length + " prakriyas</strong>";
+            restable += "<br>Please click on a prakriya below to expand/collapse";
+            $("#reshead").text("Prakriyaa");
+            var panelID = 0;
+            result.forEach(function (res) {
+                restable += createPanel(res, panelID);
+                panelID += 1;
+            });
+        } else if (result.hasOwnProperty("error")) {
+            $("#reshead").text("Error");
+            restable += result.error;
+        }
+        $("#restable").append(restable);
+        $("#jsonButton").removeClass("d-none");
+        
+        var link = $.query.set("input_trans", inputTrans).set("input", inputText).set("output_trans", outputTrans);
+        $("#linkp").text(location.pathname + link.toString());
+        $("#linktab").removeClass("d-none");
+    })
+    .fail(function () {
+        var msg = "An error occured.";
+        msg += "This may be caused by the input form not being present in the database. Please report any issues <a href=\"https://github.com/drdhaval2785/prakriya/issues\"  target=\"_blank\" rel=\"noopener noreferrer\">here</a>.";
+        $("#restable").html(msg);
+    })
+    .always(function () {
+        btn.removeClass("btn-secondary").addClass("btn-primary");
+        btn.text("Go");
+        $("#devtab").removeClass("d-none");
+        $("#restab").removeClass("d-none");
+    });
+}
+    
 $(document).ready(function () {
     "use strict";
     $("#goButton").on("click", function () {
         var inputTrans = $("#inputTrans").val();
-        var urlbase = $.query.get("api_url_base") !== "" ? $.query.get("api_url_base")
-            : "https://api.sanskritworld.in/v0.0.2/verbforms/" + inputTrans + "/";
-        var btn = $(this);
-        btn.removeClass("btn-primary").addClass("btn-secondary");
-        btn.text("Loading ...");
         var inputText = $("#inputText").val();
         var outputTrans = $("#outputTrans").val();
-        var url = urlbase + inputText + "?output_transliteration=" + outputTrans;
-        $("#devinp").text(inputText);
-        $("#jsonbox").text("");
-        $("#jsonButton").addClass("d-none");
-        $("#restab").addClass("d-none");
-        $("#restable").html("");
-        $.getJSON(url, function (result) {
-            var s = JSON.stringify(result);
-            $("#jsonbox").text(s);
-            $("#restable").html("");
-            //console.log(result);
-            var restable = "";
-            if (Array.isArray(result)) {
-                restable += "<strong>Found " + result.length + " prakriyas</strong>";
-                restable += "<br>Please click on a prakriya below to expand/collapse";
-                $("#reshead").text("Prakriyaa");
-                var panelID = 0;
-                result.forEach(function (res) {
-                    restable += createPanel(res, panelID);
-                    panelID += 1;
-                });
-            } else if (result.hasOwnProperty("error")) {
-                $("#reshead").text("Error");
-                restable += result.error;
-            }
-            $("#restable").append(restable);
-            $("#jsonButton").removeClass("d-none");
-        })
-        .fail(function () {
-            var msg = "An error occured.";
-            msg += "This may be caused by the input form not being present in the database. Please report any issues <a href=\"https://github.com/drdhaval2785/prakriya/issues\"  target=\"_blank\" rel=\"noopener noreferrer\">here</a>.";
-            $("#restable").html(msg);
-        })
-        .always(function () {
-            btn.removeClass("btn-secondary").addClass("btn-primary");
-            btn.text("Go");
-            $("#devtab").removeClass("d-none");
-            $("#restab").removeClass("d-none");
-        });
+        getPrakriya(inputTrans, inputText, outputTrans);
+        
     });
 
     // use custom plugin
     $("#inputText").enterKey(function () {
         $("#goButton").click();
     });
+    
+    var inputTrans = $.query.get("input_trans");
+    var inputText = $.query.get("input");
+    var outputTrans = $.query.get("output_trans");
+    if(inputTrans !== "" && inputText !== "" && outputTrans !== "") {
+        $("#inputTrans").val(inputTrans);
+        $("#inputText").val(inputText);
+        $("#outputTrans").val(outputTrans);
+        getPrakriya(inputTrans, inputText, outputTrans);
+    }
 
 });
